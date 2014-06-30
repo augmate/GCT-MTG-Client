36d864dcce4807119f31d7e09091d91c18bca45f
Johny Urgiles <johny@augmate.com>
2014-06-30 18:38:47 -0400

    init base template project

---
+++ app/src/main/java/com/augmate/gct_mtg_client/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.augmate.gct_mtg_client.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
