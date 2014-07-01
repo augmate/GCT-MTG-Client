34eba5e08381efa8b065e671949f0b833966318d
Johny Urgiles <johny@augmate.com>
2014-07-01 16:02:49 -0400

    Launch scanner on app start

--- app/src/main/java/com/augmate/gct_mtg_client/app/MainActivity.java
+++ app/src/main/java/com/augmate/gct_mtg_client/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.augmate.gct_mtg_client.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import com.augmate.gct_mtg_client.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new IntentIntegrator(this).initiateScan(IntentIntegrator.QR_CODE_TYPES);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IntentIntegrator.REQUEST_CODE && resultCode == RESULT_OK) {
+            IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+            TextView roomNumberView = (TextView) findViewById(R.id.room_number);
+
+            roomNumberView.setText(intentResult.getContents());
+        }
     }
 }
