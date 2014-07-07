58fe6e4cc3385d9255a1b6b56661d8bccfccb6ff
Johny Urgiles <johny@augmate.com>
2014-07-07 12:34:22 -0400

    edited the authorize.xml for a neater interface

--- app/src/main/java/com/augmate/gct_mtg_client/app/DeviceAuthInfoActivity.java
+++ app/src/main/java/com/augmate/gct_mtg_client/app/DeviceAuthInfoActivity.java
@@ -2,6 +2,7 @@ package com.augmate.gct_mtg_client.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.TextView;
 import com.augmate.gct_mtg_client.R;
 
@@ -9,6 +10,7 @@ public class DeviceAuthInfoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.authorize_device);
 
         ((TextView)findViewById(R.id.device_auth_url)).setText(getIntent().getStringExtra("verification_url"));
