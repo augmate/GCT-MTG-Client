3b2d2c0c6bfab2de9ccc66b4c0fbe7e38499e693
Johny Urgiles <johny@augmate.com>
2014-07-09 14:16:33 -0400

    have welcome screen up for 5 seconds before transitioning to scanner

--- app/src/main/java/com/augmate/gct_mtg_client/app/WelcomeActivity.java
+++ app/src/main/java/com/augmate/gct_mtg_client/app/WelcomeActivity.java
@@ -1,8 +1,10 @@
 package com.augmate.gct_mtg_client.app;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import com.augmate.gct_mtg_client.R;
+import com.google.zxing.integration.android.IntentIntegrator;
 import roboguice.activity.RoboActivity;
 
 public class WelcomeActivity extends RoboActivity{
@@ -14,6 +16,12 @@ public class WelcomeActivity extends RoboActivity{
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new IntentIntegrator(WelcomeActivity.this).initiateScan(IntentIntegrator.QR_CODE_TYPES);
+            }
+       }, 5000);
 
     }
 }
