e34015079db946b29e9c75059d9aad000a0e9134
Johny Urgiles <johny@augmate.com>
2014-07-21 19:08:14 -0400

    Update tap screen to show closest room

--- app/src/main/java/com/augmate/gct_mtg_client/app/activities/BeaconRoomSelectionActivity.java
+++ app/src/main/java/com/augmate/gct_mtg_client/app/activities/BeaconRoomSelectionActivity.java
@@ -1,25 +1,27 @@
 package com.augmate.gct_mtg_client.app.activities;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.augmate.gct_mtg_client.R;
 import com.augmate.gct_mtg_client.app.Beaconizer;
-import com.augmate.gct_mtg_client.app.utils.Log;
-import com.augmate.gct_mtg_client.app.tasks.IReceiveRoomsCallbacks;
 import com.augmate.gct_mtg_client.app.RoomOption;
-import com.segment.android.TrackedActivity;
+import com.augmate.gct_mtg_client.app.tasks.IReceiveRoomsCallbacks;
+import com.augmate.gct_mtg_client.app.utils.Log;
+import roboguice.inject.ContentView;
 
 import java.util.List;
 
-public class BeaconRoomSelectionActivity extends TrackedActivity implements IReceiveRoomsCallbacks {
+@ContentView(R.layout.activity_nearest_room)
+public class BeaconRoomSelectionActivity extends TrackedGuiceActivity implements IReceiveRoomsCallbacks {
     Beaconizer newBeaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        setContentView(R.layout.activity_nearest_room);
 
         newBeaconManager = new Beaconizer(this, this);
         Log.debug("BeaconManager configured.");
@@ -28,24 +30,18 @@ public class BeaconRoomSelectionActivity extends TrackedActivity implements IRec
     public void onReceiveNearbyRooms(List<RoomOption> rooms) {
         if (rooms.size() < 1) {
             ((TextView) findViewById(R.id.room_name)).setText("No rooms near by");
-            ((TextView) findViewById(R.id.distance)).setText("Go for a walk");
             return;
         }
 
         ((TextView) findViewById(R.id.room_name)).setText("Near: " + rooms.get(0).name);
-        ((TextView) findViewById(R.id.distance)).setText("Distance = " + String.format("%.2f", rooms.get(0).distance) + "magical units");
+    }
 
-        // always take the first two rooms
-        ((TextView) findViewById(R.id.beacon1)).setText(
-                String.format("room %s confidence = %.2f distance = %.2f",
-                        rooms.get(0).name, rooms.get(0).confidence, rooms.get(0).distance
-                ));
-
-        if (rooms.size() > 1)
-            ((TextView) findViewById(R.id.beacon2)).setText(
-                    String.format("room %s confidence = %.2f distance = %.2f",
-                            rooms.get(1).name, rooms.get(1).confidence, rooms.get(1).distance
-                    ));
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+            Toast.makeText(this, "Tap", Toast.LENGTH_SHORT).show();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
